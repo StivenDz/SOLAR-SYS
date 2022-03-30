6c940dfdef82217f33fb5686a7bd71e24e981948
StivenDz <stivendiazh@gmail.com>
2022-03-30 10:01:19 -0500

    texture,sass,animations folder added

---
+++ src/animation/nav.js
@@ -0,0 +1,34 @@
+const arrow1 = document.querySelector('.degree--up-0');
+const arrow2 = document.querySelector('.degree--up-1');
+const arrow3 = document.querySelector('.degree--up-2');
+
+const menu = document.querySelector('#menu__active');
+
+const linksNames1 = document.querySelector('.planets1');
+const linksNames2 = document.querySelector('.planets2');
+const linksNames3 = document.querySelector('.planets3');
+
+const contPlanets = document.querySelector('.planets-names');
+
+menu.toggleAttribute('checked');
+menu.addEventListener('click',(e) =>{
+    contPlanets.classList.toggle('novisible');
+    contPlanets.classList.toggle('animation');
+    contPlanets.classList.toggle('animation_reverse');
+});
+
+arrow1.addEventListener('click',(e)=>{
+    changeLinksNames('none','flex','none');
+})
+arrow2.addEventListener('click',(e)=>{
+    changeLinksNames('flex','none','none');
+})
+arrow3.addEventListener('click',(e)=>{
+    changeLinksNames('none','none','flex');
+})
+
+const changeLinksNames = (none1,none2,none3) =>{
+    linksNames3.style.display = none1;
+    linksNames2.style.display = none2;
+    linksNames1.style.display = none3;
+}
