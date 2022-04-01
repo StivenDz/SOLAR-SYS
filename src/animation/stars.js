ddfd37154292fcd76278e7eb6d2475157f38815c
StivenDz <stivendiazh@gmail.com>
2022-03-31 20:48:34 -0500

    problems with github pages in routes

--- src/animation/stars.js
+++ src/animation/stars.js
@@ -25,7 +25,7 @@ function init() {
     starGeo.vertices.push(star);
     }
 
-    let sprite = new THREE.TextureLoader().load( '/src/images/star.png' );
+    let sprite = new THREE.TextureLoader().load( '../images/star.png' );
     let starMaterial = new THREE.PointsMaterial({
     color: 0xaaaaaa,
     size: 0.7,
