ac7efecbff6d3fe5890d379924443025f83441b1
StivenDz <stivendiazh@gmail.com>
2022-04-13 01:09:53 -0500

    responsive tables

--- src/animation/tables.js
+++ src/animation/tables.js
@@ -1,9 +1,14 @@
 const tables = document.querySelector('.tables');
 const buttonTables = document.getElementById('btn-tables');
+const buttonTablesRes = document.getElementById('btn-tables-res');
 const buttonTablesClose = document.querySelector('.close-tables');
 
 
 buttonTables.addEventListener('click',(e)=>{
+    tables.style.opacity = '1';
+    tables.classList.add('pointer-event');
+})
+buttonTablesRes.addEventListener('click',(e)=>{
     tables.style.opacity = '1';
     tables.classList.add('pointer-event');
 })
